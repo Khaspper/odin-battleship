eaf774250b1a2efac4ced640a5c5fd9ac6abc4fa
Mark <brandonnarciso24@gmail.com>
2025-06-08 22:20:17 -0700

    chore:initial commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {},
+};
---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Simulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
